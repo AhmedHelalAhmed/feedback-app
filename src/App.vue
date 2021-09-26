13ad6f59eabae31ea1aee1a16ab60af6b0a9da45
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2021-09-26 05:19:34 +0200

    Sending and Getting data from firebase backend

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <learning-survey @survey-submit="storeSurvey"></learning-survey>
-  <user-experiences :results="savedSurveyResults"></user-experiences>
+  <learning-survey></learning-survey>
+  <user-experiences></user-experiences>
 </template>
 
 <script>
@@ -10,24 +10,14 @@ import UserExperiences from './components/survey/UserExperiences.vue';
 export default {
   components: {
     LearningSurvey,
-    UserExperiences,
+    UserExperiences
   },
+
   data() {
-    return {
-      savedSurveyResults: [],
-    };
+    return {};
   },
-  methods: {
-    storeSurvey(surveyData) {
-      const surveyResult = {
-        name: surveyData.userName,
-        rating: surveyData.rating,
-        id: new Date().toISOString(),
-      };
-      this.savedSurveyResults.push(surveyResult);
-      console.log(surveyResult);
-    },
-  },
+
+  methods: {}
 };
 </script>
 
